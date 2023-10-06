aebd8939d1aef8d60d1961912d3f8a991c85ef02
Zachary Dechant <zdechant0621@gmail.com>
2023-10-06 16:34:57 -0600

    Readd

---
+++ src/MakeChange/Register.java
@@ -0,0 +1,17 @@
+package MakeChange;
+
+import java.util.Scanner;
+
+public class Register {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("Please enter the price of the item: ");
+		double price = sc.nextDouble();
+		System.out.println(price);
+	}
+
+}
